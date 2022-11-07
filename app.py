63eb7c34568169c0a0b877b1a55e7eddbcacd041
Umer <UMERSHAHID667@gmail.com>
2022-11-07 16:00:12 +0500

    Add existing file

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def index():
 
 
 
-@app.route("/whatsapi", methods=["GET", "POST"])
+@app.route("/webhook", methods=["GET"])
 def hook():
     if request.method == "GET":
         if request.args.get("hub.verify_token") == VERIFY_TOKEN:
@@ -36,33 +36,7 @@ def hook():
         return "Invalid verification token"
 
 
-    data = request.get_json()
-    changed_field = messenger.changed_field(data)
-    if changed_field == "messages":
-        new_message = messenger.get_mobile(data)
-        if new_message:
-            mobile = messenger.get_mobile(data)
-            message_type = messenger.get_message_type(data)
 
-            if message_type == "text":
-                message = messenger.get_message(data)
-                name = messenger.get_name(data)
-                print(f"{name} with this {mobile} number sent  {message}")
-                messenger.send_message(f"Hi {name}, nice to connect with you", mobile)
-
-            elif message_type == "interactive":
-                message_response = messenger.get_interactive_response(data)
-                print(message_response)
-
-            else:
-                pass
-        else:
-            delivery = messenger.get_delivery(data)
-            if delivery:
-                print(f"Message : {delivery}")
-            else:
-                print("No new message")
-    return "ok"
 
 
 
